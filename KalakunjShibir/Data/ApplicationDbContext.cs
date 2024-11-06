fe719e6e182f46d7830aba8a4a0ba8aef3e6958b
milan <milanvaghasiya5225@gmail.com>
2024-11-06 12:45:15 +0530

    updated working code

--- KalakunjShibir/Data/ApplicationDbContext.cs
+++ KalakunjShibir/Data/ApplicationDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using KalakunjShibir.Models;
-using KalakunjShibir.Models.Entities;
 
 namespace KalakunjShibir.Data
 {
@@ -15,7 +14,6 @@ namespace KalakunjShibir.Data
         public DbSet<DataEntry> DataEntries { get; set; }
         public DbSet<Building> Buildings { get; set; }
         public DbSet<RoomBooking> RoomBookings { get; set; }
-        public DbSet<BookingHistory> BookingHistory { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -23,10 +21,10 @@ namespace KalakunjShibir.Data
 
             // Configure relationships
             builder.Entity<DataEntry>()
-                .HasOne(de => de.Building)
-                .WithMany(b => b.DataEntries)
-                .HasForeignKey(de => de.BuildingId)
-                .OnDelete(DeleteBehavior.Restrict);
+       .HasOne(de => de.Building)
+       .WithMany(b => b.DataEntries)  // Assuming Building has a collection of DataEntries; if not, change to .WithMany()
+       .HasForeignKey(de => de.BuildingId)
+       .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<RoomBooking>()
                 .HasOne(rb => rb.DataEntry)
@@ -36,44 +34,23 @@ namespace KalakunjShibir.Data
 
             // Seed initial buildings
             builder.Entity<Building>().HasData(
-                new Building
-                {
-                    Id = 1,
-                    Name = "Block A",
-                    TotalRooms = 50,
-                    Location = "East Wing",
-                    Description = "Building A in the East Wing",
-                    ContactPerson = "Admin 1",
-                    ContactNumber = "1234567890",
-                    IsActive = true
-                },
-                new Building
-                {
-                    Id = 2,
-                    Name = "Block B",
-                    TotalRooms = 50,
-                    Location = "West Wing",
-                    Description = "Building B in the West Wing",
-                    ContactPerson = "Admin 2",
-                    ContactNumber = "9876543210",
-                    IsActive = true
-                }
-            );
-
-            // Indexes for better query performance
-            builder.Entity<DataEntry>()
-                .HasIndex(de => de.SNo)
-                .IsUnique();
-
-            builder.Entity<DataEntry>()
-                .HasIndex(de => new { de.StartDate, de.EndDate });
-
-            builder.Entity<RoomBooking>()
-                .HasIndex(rb => new { rb.DataEntryId, rb.RoomNumber })
-                .IsUnique();
-
-            builder.Entity<RoomBooking>()
-                .HasIndex(rb => new { rb.StartDate, rb.EndDate, rb.Status });
+     new Building
+     {
+         Id = 1,
+         Name = "Block A",
+         TotalRooms = 50,
+         Location = "East Wing",
+         Description = "Building A in the East Wing"
+     },
+     new Building
+     {
+         Id = 2,
+         Name = "Block B",
+         TotalRooms = 50,
+         Location = "West Wing",
+         Description = "Building B in the West Wing"
+     }
+ );
         }
     }
 }
